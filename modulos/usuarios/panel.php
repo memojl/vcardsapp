fa8b80b7f0946ab7dfdb34d2073b14e49d720f02
memo-azell <memo.jimenez@azell.co>
2020-08-13 19:48:13 -0500

    update 13.08.2020

--- modulos/usuarios/panel.php
+++ modulos/usuarios/panel.php
@@ -1,1 +1,17 @@
-<div>Panel de usuarios</div>
+<?php
+if(isset($_SESSION["username"])){
+	if($_SESSION["level"]>=-1 && $_SESSION["level"]<=6){
+		if($_SESSION["level"]==-1){
+			$user_type='Administrador';
+        	$user_access='admin/index';
+        	$adminLTE='<a href="'.$page_url.'index.php?mod=dashboard">AdminLTE</a>';
+		}else{
+			$user_type='Miembro';
+			$user_access='miembros/index';
+		}
+        
+		include_once ('panel/pages/panel.html');
+		
+	}else{echo '<div id="cont-user">No tiene permiso para ver esta secci&oacute;n.</div>';}
+}else{header("Location: ".$page_url."index.php");}
+?>
--- temas/vcard2/menu/menu.php
+++ temas/vcard2/menu/menu.php
@@ -12,9 +12,9 @@ echo ucfirst($mod).' | '.$title;
 function login(&$login_se,$username,$ID_login){
 global $mysqli,$DBprefix,$page_url,$mod,$ext,$opc,$path_tema,$tema_previo,$dboard;
  if(isset($_SESSION['username'])){
-	$login_se='<a href="'.$page_url.'index.php?mod='.$dboard.'"><i class="fa fa-sign-in"></i> '.$username.'</a> <a href="'.logout($ID_login).'" title="Salir"><i class="fa fa-power-off"></i></a>';
+	$login_se='<li class="dropdown dropdown-primary"><a class="dropdown-toggle nav-link bg-link" href="'.$page_url.'index.php?mod='.$dboard.'"><i class="fa fa-user"></i> - '.$username.'</a> <ul class="dropdown-menu"><li><a class="class="dropdown-item"" href="'.logout($ID_login).'" title="Salir"><i class="fa fa-power-off"></i> - Salir</a></li><ul></li>';
  }else{
-	$login_se='<a href="'.$page_url.'admin/"><i class="fa fa-sign-in"></i> Login</a>';
+	$login_se='<li><a class="nav-link bg-link" href="'.$page_url.'admin/"><i class="fa fa-sign-in"></i> Login</a></li>';
  }
 }
 
