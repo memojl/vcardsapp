bd992bc398bfbbd4e88a4e618c0c192982d78502
memo-azell <memo.jimenez@azell.co>
2020-08-30 22:15:40 -0500

    update front login

--- modulos/login/index.php
+++ modulos/login/index.php
@@ -1,6 +1,6 @@
 <!--?php include '../../admin/conexion.php';?-->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es-MX">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +8,10 @@
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Startmin - Bootstrap Admin Theme</title>
+        <title>Login - <?php echo $page_name;?></title>
+        <!-- LINK-ICON -->
+        <link rel="shortcut icon" type="image/x-icon" href="<?php echo $page_url;?>favicon.ico">
+        <link rel="icon" type="image/x-icon" href="<?php echo $page_url;?>favicon.ico">
 
         <!-- Bootstrap Core CSS -->
         <link href="<?php echo $page_url.'modulos/usuarios/panel/';?>css/bootstrap.min.css" rel="stylesheet">
@@ -36,7 +39,7 @@
                 <div class="col-md-4 col-md-offset-4">
                     <div class="login-panel panel panel-default">
                         <div class="panel-heading">
-                            <h3 class="panel-title">Please Sign In</h3>
+                            <h3 class="panel-title">Login</h3>
                         </div>
                         <div class="panel-body">
                             <form role="form">
@@ -49,13 +52,17 @@
                                     </div>
                                     <div class="checkbox">
                                         <label>
-                                            <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                            <input name="remember" type="checkbox" value="Recordarme">Recordarme
                                         </label>
                                     </div>
                                     <!-- Change this to a button or input when using this as a form -->
-                                    <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a>
+                                    <a href="#" class="btn btn-lg btn-success btn-block">Ingrese</a>
                                 </fieldset>
                             </form>
+                        </div>
+                        <div class="panel-footer text-center">
+                            <a href="<?php echo $page_url;?>">Inicio</a> | <a href="#">Recuperar Contrase&ntilde;a</a><br>
+                            No tiene una cuenta registrese <a href="<?php echo $page_url;?>usuarios/registro/">aqu&iacute;.</a>
                         </div>
                     </div>
                 </div>
