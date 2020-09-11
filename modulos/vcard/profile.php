e9c2377306f620d96d80d5fec63b5ea117f5188b
memo-azell <memo.jimenez@azell.co>
2020-09-11 13:46:41 -0500

    pre-update 11092020

--- modulos/vcard/profile.php
+++ modulos/vcard/profile.php
@@ -2,7 +2,8 @@
 $perfil=$idp;
 echo $bootstrap;
 if($perfil!='' && $perfil!=NULL){
-
+    
+    include 'profile.html';
 }else{
     echo '<div class="alert alert-danger"><b>Error 404:</b> Perfil No encontrado</div>';
 }
