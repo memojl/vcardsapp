83fc0bce6e20c02822b407d2cb66544f9d4a98d6
memo-azell <memo.jimenez@azell.co>
2020-09-11 14:50:58 -0500

    profile json

--- modulos/vcard/profile.php
+++ modulos/vcard/profile.php
@@ -2,7 +2,23 @@
 $perfil=$idp;
 echo $bootstrap;
 if($perfil!='' && $perfil!=NULL){
-    
+    $row=query_row($tabla='vcard',$campo='profile',$idp);
+    $ID=$row['ID'];
+    $cover=$row['cover'];
+    $profile=$row['profile'];
+    $nombre=$row['nombre'];
+    $puesto=$row['puesto'];
+    $empresa=$row['empresa'];
+    $tel=$row['tel'];
+    $tel_ofi=$row['tel_ofi'];
+    $cell=$row['cell'];
+    $email=$row['email'];
+    $web=$row['web'];
+    $fb=$row['fb'];
+    $tw=$row['tw'];
+    $lk=$row['lk'];
+    $ins=$row['ins'];
+    $visible=$row['visible'];
     include 'profile.html';
 }else{
     echo '<div class="alert alert-danger"><b>Error 404:</b> Perfil No encontrado</div>';
